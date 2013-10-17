82743914c2556f38f49415f95982de8692a82261
Mystrocoolio <mystrocoolio@gmail.com>
2013-10-17 13:23:47 -0400

    Update Functions.c
    Commented the code and changes some stuff around (like functions now work by degree and not rotation)

--- Functions.c
+++ Functions.c
@@ -3,97 +3,131 @@
 #pragma config(Sensor, dgtl1,  rightEncoder,   sensorQuadEncoder)
 #pragma config(Sensor, dgtl3,  leftEncoder,    sensorQuadEncoder)
 
+// Feel free to rename these in the "Motors and Sensors Setup" window under "Robot" BUT BEWARE!!
+// 	You would need to change their names in almost every function below (your call tho)
+
+int InToDegrees(float in) {
+	// CHANGE THIS TO MATCH YOUR ROBOT!!
+	return in * 000; // Replace 000 with your robot's unique degree per inch
+			 // Like if your robot goes 11 inches in one full rotation, put in "(360/11)"
+}
+
 void drive(int left,int right){
+	// CHANGE THIS TO MATCH YOUR ROBOT!!
+	// Since every robot is different, i made this function so as long as you tell it how to drive
+	// 	the left ans right side, all of the later functions wont have to be changed
 	motor[rightMotor] = right;
-  motor[leftMotor] = left;
+  	motor[leftMotor] = left;
 }
 
-void moveForward(float rotation, int speed) {
+void moveForward(int degree, int speed) {
+	// goes forward for given degrees at given speed
 	SensorValue[rightEncoder] = 0;
-  SensorValue[leftEncoder] = 0;
+  	SensorValue[leftEncoder] = 0;
+  	
+  	drive(speed,speed);
 
-  while(SensorValue[leftEncoder] < rotation*360 && SensorValue[leftEncoder] < rotation*360){
-
-  }
+  	while(SensorValue[leftEncoder] < degree){
+		
+  	}
 
 	drive(0,0);
 }
 
-void moveBackward(float rotation, int speed){
+void moveBackward(int degree, int speed){
+	// goes backward for given degrees at given speed
 	SensorValue[rightEncoder] = 0;
-  SensorValue[leftEncoder] = 0;
+  	SensorValue[leftEncoder] = 0;
+  
+    	drive(-1*speed,-1*speed);
 
-  while(SensorValue[leftEncoder] < -1*	rotation*360){
-    drive(-1*speed,-1*speed);
-  }
+  	while(SensorValue[leftEncoder] < -1*degree){
+  		
+  	}
 
 	drive(0,0);
 }
 
-void moveLeftPoint(float rotation, int speed) {
+// ( ( (BTW) ) )
+// A point turn is when both sides run at oposite speeds to turn about the center of the robot
+// A swing turn is when one sides runs as the other doesnt to turn about the the stationary side's front wheel
+
+void moveLeftPoint(int degree, int speed) {
+	// Does a point turn left for given degrees at given speed
 	SensorValue[rightEncoder] = 0;
-  SensorValue[leftEncoder] = 0;
+  	SensorValue[leftEncoder] = 0;
+  	
+  	drive(-1*speed,speed);
 
-  while(SensorValue[rightEncoder] < rotation*360){
-    drive(-1*speed,speed);
-  }
+  	while(SensorValue[rightEncoder] < degree){
+  	}
 
 	drive(0,0);
 }
 
-void moveRightPoint(float rotation, int speed) {
+void moveRightPoint(int degree, int speed) {
+	// Does a point turn right for given degrees at given speed
 	SensorValue[rightEncoder] = 0;
-  SensorValue[leftEncoder] = 0;
+  	SensorValue[leftEncoder] = 0;
+  	
+  	drive(speed,-1*speed);
 
-  while(SensorValue[leftEncoder] < rotation*360){
-    drive(speed,-1*speed);
-  }
+  	while(SensorValue[leftEncoder] < degree){
+    		
+  	}
 
 	drive(0,0);
 }
 
-void moveLeftSwing(float rotation, int speed) {
+void moveLeftSwing(int degree, int speed) {
+	// Does a swing turn left for given degrees at given speed
 	SensorValue[rightEncoder] = 0;
-  SensorValue[leftEncoder] = 0;
+  	SensorValue[leftEncoder] = 0;
+  	
+    	drive(0,speed);
 
-  while(SensorValue[rightEncoder] < rotation*360){
-    drive(0,speed);
-  }
-
+  	while(SensorValue[rightEncoder] < degree){
+  	}
+		
 	drive(0,0);
 }
 
-void moveRightSwing(float rotation, int speed) {
+void moveRightSwing(int degree, int speed) {
+	// Does a swing turn right for given degrees at given speed
 	SensorValue[rightEncoder] = 0;
-  SensorValue[leftEncoder] = 0;
+  	SensorValue[leftEncoder] = 0;
+  	
+  	drive(speed,0);
 
-  while(SensorValue[leftEncoder] < rotation*360){
-    drive(speed,0);
-  }
+  	while(SensorValue[leftEncoder] < degree){
+    		
+  	}
 
 	drive(0,0);
 }
 
-void wait(int time) {
+void stopSec(float time) {
+	// simply stops the motors and waits for the given amount of seconds
+	// I though seconds whould be better to work with than milliseconds, and feel free to put in decimals
 	drive(0,0);
-	wait1Msec(time);
+	wait1Msec(time*1000);
 }
 
 void dance(int reps) {
+	// Just something to test the functions and provide amusement, totaly useless otherwise
 	for(int i = 1; i <=reps; i++) {
-		moveLeftPoint(.25,110);
-		wait(800);
-		moveRightPoint(.25,110);
-		wait(800);
-		moveRightPoint(.25,110);
-		wait(800);
-		moveLeftPoint(.25,110);
-		wait(800);
+		moveLeftPoint(90,110);
+		stopSec(.8);
+		moveRightPoint(90,110);
+		stopSec(.8);
+		moveRightPoint(90,110);
+		stopSec(.8);
+		moveLeftPoint(90,110);
+		stopSec(.8);
 	}
 }
 
 
 task main() {
-	wait1Msec(2000);
-  dance(5);
+	
 }
